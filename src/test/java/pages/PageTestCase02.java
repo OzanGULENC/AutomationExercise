c66a64f35ff70b5e93c84053ed43015070b28c9b
Tevfik Aykaç <tevfikaykac1938@gmail.com>
2023-02-22 23:26:12 +0300

    22.02.2023TA

---
+++ src/test/java/pages/PageTestCase02.java
@@ -0,0 +1,14 @@
+package pages;
+
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class PageTestCase02 {
+    public PageTestCase02() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+}
+
---
+++ target/test-classes/features/TestCase02.feature
@@ -0,0 +1,5 @@
+Feature: TestCase02
+@Tevfik
+   Scenario: Login User with correct email and password
+    Given Tevfik Navigate to url
+    When Tevfik Verify that home page is visible successfully
