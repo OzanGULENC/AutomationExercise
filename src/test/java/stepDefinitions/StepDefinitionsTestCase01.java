ce4df4b72b31c61717361091100b58e27e171a9a
ozangulenc <ozangulenc90@gmail.com>
2023-02-22 18:04:19 +0300

    AE01.02

--- src/test/java/stepDefinitions/StepDefinitionsTestCase01.java
+++ src/test/java/stepDefinitions/StepDefinitionsTestCase01.java
@@ -6,6 +6,9 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import pages.PageTestCase01;
 import utilities.ConfigReader;
 import utilities.Driver;
@@ -17,6 +20,8 @@ public class StepDefinitionsTestCase01 {
 
     PageTestCase01 pageTC01=new PageTestCase01();
     Faker faker=new Faker();
+    Actions actions=new Actions(Driver.getDriver());
+    JavascriptExecutor jse=(JavascriptExecutor) Driver.getDriver();
 
     @Given("Navigate to url")
     public void navigateToUrl() {
@@ -70,16 +75,37 @@ public class StepDefinitionsTestCase01 {
     }
 
     @And("Fill details: Title, Name, Email, Password, Date of birth")
-    public void fillDetailsTitleNameEmailPasswordDateOfBirth() {
+    public void fillDetailsTitleNameEmailPasswordDateOfBirth(){
+        pageTC01.accountInformationTitleMRButton.click();
+        // String accountName=faker.name().firstName();
+        // System.out.println(accountName);
+        // pageTC01.accountInformationNameBox.sendKeys(accountName);
+        // String accountEmail=faker.internet().emailAddress();
+        // System.out.println(accountEmail);
+        // pageTC01.accountInformationEmailBox.sendKeys(accountEmail);
+        String accountPassword=faker.internet().password();
+        System.out.println(accountPassword);
+        pageTC01.accountInformationPasswordBox.sendKeys(accountPassword);
+        Select selectBirthDayDropDown=new Select(pageTC01.accountInformationBirthDay);
+        selectBirthDayDropDown.selectByIndex(10);
+        Select selectBirthMonthDropDown=new Select(pageTC01.accountInformationBirthMonth);
+        selectBirthMonthDropDown.selectByIndex(10);
+        Select selectBirthYearDropDown=new Select(pageTC01.accountInformationBirthYear);
+        selectBirthYearDropDown.selectByValue("2000");
+
 
     }
 
     @And("Select checkbox Sign up for our newsletter!")
     public void selectCheckboxSignUpForOurNewsletter() {
+        jse.executeScript("arguments[0].click();",pageTC01.accountInformationNewsletterCheckBox);
+
     }
 
     @And("Select checkbox Receive special offers from our partners")
     public void selectCheckboxReceiveSpecialOffersFromOurPartners() {
+        jse.executeScript("arguments[0].click();",pageTC01.accountInformationReceiveSpecialCheckBox);
+
     }
 
     @And("Fill details: First name, Last name, Company, Address, Address{int}, Country, State, City, Zipcode, Mobile Number")
