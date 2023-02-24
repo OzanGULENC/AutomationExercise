097c82706e97a3b5c333120ab51cb053389457ae
ozangulenc <ozangulenc90@gmail.com>
2023-02-24 22:33:45 +0300

    AE01.04

---
+++ src/test/java/stepDefinitions/StepDefinitionsTestCase03.java
@@ -0,0 +1,30 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class StepDefinitionsTestCase03 {
+
+
+
+    @And("Verify Login to your account is visible")
+    public void verifyLoginToYourAccountIsVisible() {
+
+    }
+
+    @And("Enter incorrect email address and password")
+    public void enterIncorrectEmailAddressAndPassword() {
+
+    }
+
+    @And("Click login button")
+    public void clickLoginButton() {
+
+    }
+
+    @And("Verify error Your email or password is incorrect is visible")
+    public void verifyErrorYourEmailOrPasswordIsIncorrectIsVisible() {
+
+    }
+
+
+}
